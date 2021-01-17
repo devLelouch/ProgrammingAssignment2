3a15abf760e91fff514d8a481a1ed0f2e3ff2182
devLelouch <angelzam.zambrano@gmail.com>
2021-01-16 20:48:15 -0400

    Create class makeCacheMatrix and its methods.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a special class of objects that stores a numeric matrix and
+## cache its inverse.
+ 
 
-## Write a short comment describing this function
+## defines the class makeCacheMatrix with its
+## attributes, constructor, setters and getters 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(cacheMat = matrix()) {
+  matInverse <- NULL
+  set <- function(y) {
+    cacheMat <<- y
+    matInverse <<- NULL              # if modify the matrix, clear cache 
+  }
+  get <- function() cacheMat
+  setInverse <- function(newInverse) matInverse <<- newInverse  
+  getInverse <- function() matInverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cache the inverse of cacheMat or return the value cached previously.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matr, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  matInverse <- matr$getInverse()
+  if(!is.null(matInverse)) {
+    message("getting cached data")
+    return(matInverse)
+  }
+  cacheMat <- matr$get()
+  matInverse <- solve(cacheMat, ...)
+  matr$setInverse(matInverse)
+  matInverse
 }
